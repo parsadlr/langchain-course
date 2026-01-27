d3f8392382f68c9f28f8894a2ffec63d2bd93d30
Parsa Delavari <delavari.p@gmail.com>
2026-01-26 17:05:41 -0800

    environment setup

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+print(OPENAI_API_KEY)
+
+
+def main():
+    print("Hello from langchain-course!")
+    print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
